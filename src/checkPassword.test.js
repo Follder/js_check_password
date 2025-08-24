cb41dbe7595ac86a58bf5f66f7729386b2bbfe27
Dmytro Koriahin <dkoryahin@gmail.com>
2025-08-24 19:23:14 +0300

    add task solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,32 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
+    const r = checkPassword('');
 
+    expect(typeof r).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
-
+    expect(checkPassword('Password1!')).toEqual(true);
   });
 
-  // write more tests here
+  it(`should return 'false' if chars less than 8`, () => {
+    expect(checkPassword('qwerty')).toEqual(false);
+  });
+
+  it(`should return 'false' if chars 16 or more`, () => {
+    expect(checkPassword('Password1!Password1!')).toEqual(false);
+  });
+
+  it(`should return 'false' if we haven't uppercase letter`, () => {
+    expect(checkPassword('password1!')).toEqual(false);
+  });
+
+  it(`should return 'false' if we haven't digit`, () => {
+    expect(checkPassword('Passwordd!')).toEqual(false);
+  });
+
+  it(`should return 'false' if we haven't special symbol`, () => {
+    expect(checkPassword('Password11')).toEqual(false);
+  });
 });
